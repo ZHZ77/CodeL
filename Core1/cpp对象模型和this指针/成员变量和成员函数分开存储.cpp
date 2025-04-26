b1f8e1c7c0172d4726ddb8bf2b9928bbdfc89106
zhz77 <1099897459@qq.com>
2025-04-26 16:03:07 +0800

    add

---
+++ Core1/cpp对象模型和this指针/成员变量和成员函数分开存储.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//class Person
+//{
+//public:
+//	int m_A;				// 非静态成员变量		属于类的对象上
+//	static int m_B;			// 静态成员变量		不属于类的对象上
+//	void func(){}			// 非静态成员函数		不属于类的对象上
+//	static void func2(){}	// 静态成员函数		不属于类的对象上
+//};
+//
+//int Person::m_B = 10;
+//
+//void test01()
+//{
+//	Person p;
+//	cout << "size of p = " << sizeof(p) << endl;
+//}
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}
